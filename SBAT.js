e1687472bed1be88df8cfac7252f895c8445b154
CharlotteWeirich <charlotte.weirich@web.de>
2022-08-26 11:20:19 +0200

    added option to name and download file

--- SBAT.js
+++ SBAT.js
@@ -52,7 +52,28 @@ function negativeButtonClicked(){
 }
 
 function downloadButtonClicked(){
-
+    let textToWrite = outputData;
+    let textFileAsBlob = new Blob([textToWrite], {type:'application/json'});
+    let fileNameToSaveAs = document.getElementById('fileNameToSaveAs').value;
+    let downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.innerHTML = "Download File";
+    if (window.webkitURL != null)
+    {
+        // Chrome allows the link to be clicked
+        // without actually adding it to the DOM.
+        downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+    }
+    else
+    {
+        // Firefox requires the link to be added to the DOM
+        // before it can be clicked.
+        downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+        downloadLink.onclick = destroyClickedElement;
+        downloadLink.style.display = "none";
+        document.body.appendChild(downloadLink);
+    }
+    downloadLink.click();
 }
 
 function getFileData(uploadedFile){

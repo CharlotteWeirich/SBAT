7691f4c5cb90e63c03e0a1c6fba5550f1d52d8e0
CharlotteWeirich <charlotte.weirich@web.de>
2022-08-26 15:49:40 +0200

    Update SBAT.js

--- SBAT.js
+++ SBAT.js
@@ -12,74 +12,23 @@ let downloadButton = document.getElementById('downloadButton');
 let fileSelector = document.getElementById('fileSelector');
 
 
-// Main
+// Main/Setup
 setupHTMLElements();
 progressTextDisplay();
 
 function setupHTMLElements(){
-    positiveButton.addEventListener('click', positiveButtonClicked());
-    negativeButton.addEventListener('click', negativeButtonClicked());
-    downloadButton.addEventListener('click', downloadButtonClicked());
+    positiveButton.addEventListener('click', positiveButtonClicked);
+    negativeButton.addEventListener('click', negativeButtonClicked);
+    downloadButton.addEventListener('click', downloadButtonClicked);
     fileSelector.addEventListener('change', (event) => {
         getFileData(event.target.files[0]);
     });
 }
 
-function progressTextDisplay(){
-    if (textIndex < inputData.length){
-        textDisplay.textContent = inputData[textIndex];
-        textIndex ++;
-    }
-    else{
-        positiveButton.disabled = true;
-        negativeButton.disabled = true;
-        alert ('Reached end of data.');
-        displayOutput();
-        if (fileUploaded){
-            downloadButton.disabled = false;
-        }
-    }
-}
-
-function displayOutput(){
-    outputData = outputData.slice(0, -3);
-    outputData += ']';
-    textDisplay.textContent = outputData;
-}
-
-function positiveButtonClicked(){
-    outputData += '{"text": "' + inputData[textIndex-1] + '", "label": "positive"},\r\n';
-    progressTextDisplay();
-}
-
-function negativeButtonClicked(){
-    outputData += '{"text": "' + inputData[textIndex-1] + '", "label": "negative"},\r\n';
-    progressTextDisplay();
-}
-
-function downloadButtonClicked(){
-    let textFileAsBlob = new Blob([outputData], {type:'application/json'});
-    let downloadLink = document.createElement("a");
-    downloadLink.download = document.getElementById('fileNameToSaveAs').value;;
-    downloadLink.innerHTML = "Download File";
-    if (window.webkitURL != null)
-    {
-        // Chrome allows the link to be clicked
-        // without actually adding it to the DOM.
-        downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
-    }
-    else
-    {
-        // Firefox requires the link to be added to the DOM
-        // before it can be clicked.
-        downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
-        downloadLink.onclick = destroyClickedElement;
-        downloadLink.style.display = "none";
-        document.body.appendChild(downloadLink);
-    }
-    downloadLink.click();
-}
-
+// Upload Button
+/* when a user uploads a file, load the contents into inputData
+*/
+// called when change in fileSelector
 function getFileData(uploadedFile){
     downloadButton.disabled = true;
     positiveButton.disabled = false;
@@ -105,4 +54,65 @@ function getFileData(uploadedFile){
         }     
     });
     reader.readAsText(uploadedFile);
+}
+
+// Annotation
+/* 
+*/
+function positiveButtonClicked(){
+    outputData += '{"text": "' + inputData[textIndex-1] + '", "label": "positive"},\r\n';
+    progressTextDisplay();
+}
+
+function negativeButtonClicked(){
+    outputData += '{"text": "' + inputData[textIndex-1] + '", "label": "negative"},\r\n';
+    progressTextDisplay();
+}
+
+function progressTextDisplay(){
+    if (textIndex < inputData.length){
+        textDisplay.textContent = inputData[textIndex];
+        textIndex ++;
+    }
+    else{
+        positiveButton.disabled = true;
+        negativeButton.disabled = true;
+        alert ('Reached end of data.');
+        displayOutput();
+        if (fileUploaded){
+            downloadButton.disabled = false;
+        }
+    }
+}
+
+function displayOutput(){
+    outputData = outputData.slice(0, -3);
+    outputData += ']';
+    textDisplay.textContent = outputData;
+}
+
+// Download Button
+/* write outputData to a .json file and download it
+*/
+function downloadButtonClicked(){
+    let textFileAsBlob = new Blob([outputData], {type:'application/json'});
+    let downloadLink = document.createElement("a");
+    downloadLink.download = document.getElementById('fileNameToSaveAs').value;;
+    downloadLink.innerHTML = "Download File";
+    if (window.webkitURL != null)
+    {
+        // Chrome allows the link to be clicked
+        // without actually adding it to the DOM.
+        downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+    }
+    else
+    {
+        // Firefox requires the link to be added to the DOM
+        // before it can be clicked.
+        downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+        downloadLink.onclick = destroyClickedElement;
+        downloadLink.style.display = "none";
+        document.body.appendChild(downloadLink);
+    }
+    downloadLink.click();
 }

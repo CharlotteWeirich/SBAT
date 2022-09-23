e005fb6b010712959c8d9e6bd981eaff18f41e99
CharlotteWeirich <charlotte.weirich@web.de>
2022-09-23 15:23:11 +0200

    Update SBAT.js

--- SBAT.js
+++ SBAT.js
@@ -3,7 +3,7 @@ let inputData = ['This movie sucks.', 'I loved it!', 'A waste of time.',
 let textIndex = 0;
 let outputData = [];
 let labelSet = [];
-let shortcutList = [];
+let shortcutList;
 let keyCodeList = {"0":48,"1":49,"2":50,"3":51,"4":52,"5":53,"6":54,"7":55,"8":56,"9":57,"d":68,"b":66,"a":65,"s":83,"i":73,"f":70,"k":75,"ß":219,"Dead":220,"+":187,"ü":186,"p":80,"o":79,"u":85,"z":90,"t":84,"r":82,"e":69,"w":87,"g":71,"h":72,"j":74,"l":76,"ö":192,"ä":222,"#":191,"y":89,"x":88,"c":67,"v":86,"n":78,"m":77,",":188,".":190,"-":189,"ArrowRight":39,"ArrowLeft":37,"ArrowUp":38,"ArrowDown":40,"PageDown":34,"Clear":12,"Home":36,"PageUp":33,"End":35,"Delete":46,"Insert":45,"Control":17,"AltGraph":18,"Meta":92,"Alt":18,"Shift":16,"CapsLock":20,"Tab":9,"Escape":27,"F1":112,"F2":113,";":188,":":190,"_":189,"'":191,"*":187,"Q":81,"W":87,"E":69,"R":82,"T":84,"Z":90,"S":83,"A":65,"D":68,"I":73,"U":85,"O":79,"Y":89,"X":88,"C":67,"F":70,"V":86,"G":71,"B":66,"H":72,"N":78,"J":74,"M":77,"K":75,"L":76,"P":80,"Ö":192,"Ä":222,"Ü":186,"!":49,"\"":50,"§":51,"$":52,"%":53,"&":54,"/":55,"(":56,")":57,"=":48,"?":219,"°":220}
 
 // HTML Elements
@@ -98,6 +98,7 @@ function makeLabelButton(label){
 function submitButtonClicked(){
     submitButton.disabled = true;
     shortcutButton.disabled = false;
+    
     // remove the current label buttons
     if (labelSet.length > 0){
         for (let i = 0; i < labelSet.length; i++){
@@ -194,32 +195,24 @@ function shortcutButtonClicked(){
 
 function shortcutOkayButtonClicked(){
 
-    // fill the shortcutList with the entered shortcuts
-    shortcutList = [];
+    // fill the shortcutList with the entered shortcuts & delete html elements
+    shortcutList = new Object();
     for(i = 0; i < labelSet.length; i++){
         shortcutField = document.getElementById(labelSet[i] + 'Shortcut');
-        if (shortcutField.value){
-            shortcutList.push(shortcutField.value);
-        }
-        else{
-            alert ('Please fill in all the boxes!')
-            return;
-        } 
-    }
-
-    // delete the textareas and labels
-    for(i = 0; i < shortcutList.length; i++){
+        shortcutList[labelSet[i]] = shortcutField.value;
         shortcutField = document.getElementById(labelSet[i] + 'Shortcut');
         shortcutField.parentNode.removeChild(shortcutField);
         shortcutFieldLabel = document.getElementById(labelSet[i] + 'Label');
         shortcutFieldLabel.parentNode.removeChild(shortcutFieldLabel);
     }
+
     shortcutOkayButton.hidden = true;
 
     // add shortcut functionality
     document.onkeyup = function(e){
-        for(i = 0; i < shortcutList.length; i++){
-            if (e.which == keyCodeList[shortcutList[i]] || e.keyCode == keyCodeList[shortcutList[i]]){
+        for(i = 0; i < labelSet.length; i++){
+            if (e.which == keyCodeList[shortcutList[labelSet[i]]] 
+                || e.keyCode == keyCodeList[shortcutList[labelSet[i]]]){
                 document.getElementById(labelSet[i] + 'Button').click();
             }
         }

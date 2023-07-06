16e59a480b0ea860de07db626d18a12a2d6789b9
CharlotteWeirich <charlotte.weirich@web.de>
2023-07-06 13:13:58 +0200

    implemented authentication

--- SBAT.js
+++ SBAT.js
@@ -13,7 +13,7 @@ let shortcutList;
 let multilabel = false;
 let saveToLocalStorage = false;
 let keyCodeList = {"0":48,"1":49,"2":50,"3":51,"4":52,"5":53,"6":54,"7":55,"8":56,"9":57,"d":68,"b":66,"a":65,"s":83,"i":73,"f":70,"k":75,"ß":219,"Dead":220,"+":187,"ü":186,"p":80,"o":79,"u":85,"z":90,"t":84,"r":82,"e":69,"w":87,"g":71,"h":72,"j":74,"l":76,"ö":192,"ä":222,"#":191,"y":89,"x":88,"c":67,"v":86,"n":78,"m":77,",":188,".":190,"-":189,"ArrowRight":39,"ArrowLeft":37,"ArrowUp":38,"ArrowDown":40,"PageDown":34,"Clear":12,"Home":36,"PageUp":33,"End":35,"Delete":46,"Insert":45,"Control":17,"AltGraph":18,"Meta":92,"Alt":18,"Shift":16,"CapsLock":20,"Tab":9,"Escape":27,"F1":112,"F2":113,";":188,":":190,"_":189,"'":191,"*":187,"Q":81,"W":87,"E":69,"R":82,"T":84,"Z":90,"S":83,"A":65,"D":68,"I":73,"U":85,"O":79,"Y":89,"X":88,"C":67,"F":70,"V":86,"G":71,"B":66,"H":72,"N":78,"J":74,"M":77,"K":75,"L":76,"P":80,"Ö":192,"Ä":222,"Ü":186,"!":49,"\"":50,"§":51,"$":52,"%":53,"&":54,"/":55,"(":56,")":57,"=":48,"?":219,"°":220}
-let authKey = '';
+let authenticatedUser = '';
 
 // HTML Elements
 let textDisplay = document.getElementById('textDisplay');
@@ -58,7 +58,6 @@ settingSwitch.checked = false;
 wholeDocumentSwitch.checked = false;
 multilabelSwitch.checked = false;
 localStorageSwitch.checked = false;
-loadConfigFile('./config.json');
 
 if (localStorage.getItem('SBATData') != null){
     annotationArea.hidden = false;
@@ -716,8 +715,21 @@ function saveDataToLocalStorage(){
 function loadConfigFile(file){
     $.getJSON(file)
     .done(function( data ) {
-        makeFileSelection(data.annotators[0].files);
-        loadConfigSettings(data.annotators[0].settings[0]);
+        let userIndex;
+        // search for the authenticated user in the annotators list
+        for (let i = 0; i < data.annotators.length; i++){
+            if(data.annotators[i] == authenticatedUser){
+                userIndex = i;
+            }
+        }
+        if (userIndex == undefined){
+            alert("Autenticated User is not in Annotators list");
+            return;
+        }
+        else{
+            makeFileSelection(data.annotators[userIndex].files);
+            loadConfigSettings(data.annotators[userIndex].settings[0]);
+        }
     });
 }
 
@@ -800,7 +812,7 @@ function loadConfigSettings(settings){
 }
 
 async function authenticationOkayButtonClicked(){
-    authKey = personalAccessToken.value;
+    let authKey = personalAccessToken.value;
     // Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
     const octokit = new Octokit({ auth: authKey });
 
@@ -808,7 +820,10 @@ async function authenticationOkayButtonClicked(){
     const {
         data: { login },
     } = await octokit.rest.users.getAuthenticated();
+    authenticatedUser = login;
     console.log("Hello, %s", login);
+
+    loadConfigFile('./config.json');
 }
 
 //warning before closing the window

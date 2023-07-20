24ce35f89af4b74dc9abd8953229d881e2d8f7db
CharlotteWeirich <charlotte.weirich@web.de>
2023-07-20 17:20:13 +0200

    trying around

--- SBAT.js
+++ SBAT.js
@@ -14,6 +14,7 @@ let multilabel = false;
 let saveToLocalStorage = false;
 let keyCodeList = {"0":48,"1":49,"2":50,"3":51,"4":52,"5":53,"6":54,"7":55,"8":56,"9":57,"d":68,"b":66,"a":65,"s":83,"i":73,"f":70,"k":75,"ß":219,"Dead":220,"+":187,"ü":186,"p":80,"o":79,"u":85,"z":90,"t":84,"r":82,"e":69,"w":87,"g":71,"h":72,"j":74,"l":76,"ö":192,"ä":222,"#":191,"y":89,"x":88,"c":67,"v":86,"n":78,"m":77,",":188,".":190,"-":189,"ArrowRight":39,"ArrowLeft":37,"ArrowUp":38,"ArrowDown":40,"PageDown":34,"Clear":12,"Home":36,"PageUp":33,"End":35,"Delete":46,"Insert":45,"Control":17,"AltGraph":18,"Meta":92,"Alt":18,"Shift":16,"CapsLock":20,"Tab":9,"Escape":27,"F1":112,"F2":113,";":188,":":190,"_":189,"'":191,"*":187,"Q":81,"W":87,"E":69,"R":82,"T":84,"Z":90,"S":83,"A":65,"D":68,"I":73,"U":85,"O":79,"Y":89,"X":88,"C":67,"F":70,"V":86,"G":71,"B":66,"H":72,"N":78,"J":74,"M":77,"K":75,"L":76,"P":80,"Ö":192,"Ä":222,"Ü":186,"!":49,"\"":50,"§":51,"$":52,"%":53,"&":54,"/":55,"(":56,")":57,"=":48,"?":219,"°":220}
 let authenticatedUser = '';
+let octokit;
 
 // HTML Elements
 let textDisplay = document.getElementById('textDisplay');
@@ -843,7 +844,7 @@ function loadConfigSettings(settings){
 async function authenticationOkayButtonClicked(){
     let authKey = personalAccessToken.value;
     // Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
-    const octokit = new Octokit({ auth: authKey });
+    octokit = new Octokit({ auth: authKey });
 
     // Compare: https://docs.github.com/en/rest/reference/users#get-the-authenticated-user
     const {
@@ -855,9 +856,13 @@ async function authenticationOkayButtonClicked(){
     loadConfigFile('./config.json');
 }
 
-function commitButtonClicked(){
+async function commitButtonClicked(){
     //TODO: make file, upload file
     let fileToCommit = makeFile();
+
+    //first get the SHA of the last commit and its tree
+    const {data: refData} = await octokit.git.getRef;
+    console.log(refData);
 }
 
 //warning before closing the window

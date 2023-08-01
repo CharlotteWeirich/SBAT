7cf5f3ae83264330b8b1462c120f693e35f79b91
CharlotteWeirich <charlotte.weirich@web.de>
2023-08-01 13:50:24 +0200

    pls work

--- SBAT.js
+++ SBAT.js
@@ -904,7 +904,7 @@ async function commitButtonClicked(){
       // create a tree for some reason
       //('https://github.com/' + owner + '/' + repo + '/blob/commitChanges/' + fileName)
 
-    let tree = [{path: 'https://api.github.com/repos/charlotteweirich/SBAT/git/trees'},
+    let tree = [{path: './' + fileName},
         {mode: '100644'},
         {type: 'blob'},
         {sha: blobData.data.sha}];

9efca6a242221288b581b980d5de0fe25a74f243
CharlotteWeirich <charlotte.weirich@web.de>
2023-08-30 15:25:09 +0200

    fixed file selection for json files

--- SBAT.js
+++ SBAT.js
@@ -771,7 +771,26 @@ function makeFileSelection(files){
             $.get('./' + selectedFile)
             .done(function(data) {
                 content = data;
-                loadJson(content);
+                let parsedJson = content;
+                inputData = [];
+                enteredLabelSet.value = '';
+                if (parsedJson.labelSet.length > 0){
+                    for (let i = 0; i < parsedJson.labelSet.length; i++){
+                        if (i == parsedJson.labelSet.length-1){
+                            enteredLabelSet.value += parsedJson.labelSet[i]
+                        }
+                        else{
+                            enteredLabelSet.value += parsedJson.labelSet[i] + '\r\n';
+                        }
+                    }
+                    submitButtonClicked();
+                }
+
+                for (let i = 0; i < parsedJson.data.length; i++){
+                    inputData.push(parsedJson.data[i].text);
+                    outputData.push(parsedJson.data[i]);
+                }
+                progressTextDisplay();
             }); 
         }
     })
